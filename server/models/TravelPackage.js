be40b28be135e57d0ddb779ee8f9627bc2106af2
AlexG1040 <alexgarcia1040@gmail.com>
2021-08-26 19:07:12 -0700

    some changes

--- server/models/TravelPackage.js
+++ server/models/TravelPackage.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const travelpackageSchema = new Schema({
-    location: String,
-    climate: [String],
-    topography: [String],
-    airfare: Boolean,
-    tranportation: String,
-    lodging: String,
-    activity: [String],
-    cost: Number,
- });
+  location: String,
+  climate: [String],
+  topography: [String],
+  airfare: Boolean,
+  transportation: String,
+  lodging: String,
+  activity: [String],
+  cost: Number,
+});
 
 const travelpackage = mongoose.model("TravelPackage", travelpackageSchema);
 
