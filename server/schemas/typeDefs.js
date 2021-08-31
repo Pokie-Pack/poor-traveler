dc5fafdcff09a645330907218290c68508d2caad
AlexG1040 <alexgarcia1040@gmail.com>
2021-08-30 18:05:32 -0700

    updated typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,29 +3,32 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type TravelPackage {
     _id: ID
-    location: String,
-    climate: String,
-    topography: String,  
-    airfare: Boolean, 
-    transportation: String, 
-    lodging: String, 
-    activity: [String], 
-    cost: Float,
+    location: String
+    climate: String
+    topography: String
+    airfare: Boolean
+    transportation: String
+    lodging: String
+    activity: [String]
+    cost: Float
   }
 
-  type user {
-    type User {
-      _id: ID
-      firstName: String
-      lastName: String
-      email: String
-      password: String
-    }
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
   }
 
   type Query {
     user: [User]
-    travelPackage(activity: [String], location: String, climate: String, topography: String): [TravelPackage]
+    travelPackages(
+      activity: [String]
+      location: String
+      climate: String
+      topography: String
+    ): [TravelPackage]
   }
 
   type Auth {
