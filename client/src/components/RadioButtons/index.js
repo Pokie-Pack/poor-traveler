4d17491e0f8306a0282125de1cac220f63662cb6
DrewG <80792838+drewg07@users.noreply.github.com>
2021-09-01 23:51:39 -0700

    removed commented out code

--- client/src/components/RadioButtons/index.js
+++ client/src/components/RadioButtons/index.js
@@ -1,20 +1,6 @@
 import { Fragment } from "react";
 import "./style.css";
 
-/**
- * name - name attribute of the form element
- * options - array of objects that describe the options
- *            each option is an object
- *          {
- *            value: ""
- *            label: ""
- *            class: ""
- *          }
- *
- * selected - value that is selected
- * onChange - function that handles change event
- * direction - layout direction either "column" or "row"
- */
 function RadioButtons({ name, options, selected, onChange, direction }) {
   const rnd = Math.floor(Math.random() * 10000);
   return (
